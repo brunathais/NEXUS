cc67ff68dfec73d3973acafe6b84f1293f8dc399
brunathais <bruna_t_hermes@estudante.sesisenai.org.br>
2025-05-21 22:24:57 -0300

    remove comentarios java, transações funcionais com o js integradio ao html

--- back-end/src/main/java/com/ska/NEXUS/controllers/UsuarioController.java
+++ back-end/src/main/java/com/ska/NEXUS/controllers/UsuarioController.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.ska.NEXUS.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
