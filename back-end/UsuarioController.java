0fc947199059a959380789cc05e726d88898937d
CarolOestreich <caroline_oestreich@estudante.sesisenai.org.br>
2025-05-26 22:19:23 -0300

    Add files via upload

---
+++ back-end/UsuarioController.java
@@ -0,0 +1,68 @@
+package com.ska.NEXUS.controllers;
+
+import com.ska.NEXUS.dto.UsuarioDTO;
+import com.ska.NEXUS.dto.autenticacaoDTO;
+import com.ska.NEXUS.service.AutenticacaoService;
+import com.ska.NEXUS.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/usuarioController")
+@CrossOrigin(origins = "*")
+public class UsuarioController {
+
+    @Autowired
+    UsuarioService usuarioService;
+        
+   
+    @PostMapping
+    @RequestMapping("/autenticar")
+    public ResponseEntity<?> autenticar(@RequestBody autenticacaoDTO dto) {
+        AutenticacaoService service  = new AutenticacaoService();
+        
+        // ? = qualquer classe
+        //ResponseEntity<autenticacaoDTO>
+        
+        boolean autenticado = service.autenticar(dto);
+
+         System.err.println(autenticado);
+        
+        if (autenticado) {
+            return ResponseEntity.ok("Usuário autenticado com sucesso!");
+        } else {
+            return ResponseEntity.status(401).body("Usuário ou senha inválidos!");
+        }
+    }
+    
+    @GetMapping
+    @RequestMapping("/teste")
+    public String autenticar(@RequestParam String dto) {
+        return "teste " + dto;
+    }
+    
+    
+    @GetMapping
+    @RequestMapping("/cadastro")
+    public ResponseEntity<?> cadastro() {
+
+        UsuarioDTO teste = new UsuarioDTO();
+        teste.setEmail("ALGUMACOISA@GMAIL");
+        teste.setNome("POLENTA FRITA");
+        
+        System.out.println("A");
+        
+        boolean cadastrado= usuarioService.cadastro(teste);
+
+         System.err.println(cadastrado);
+        
+        if (cadastrado) {
+            return ResponseEntity.ok("Usuário autenticado com sucesso!");
+        } else {
+            return ResponseEntity.status(401).body("Usuário ou senha inválidos!");
+        }
+    }
+    
+    
+}
