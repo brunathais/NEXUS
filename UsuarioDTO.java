af0d4e064b8da2bf7d36b768c29a8fc6a357f971
CarolOestreich <caroline_oestreich@estudante.sesisenai.org.br>
2025-05-28 22:23:35 -0300

    cadastros em java

---
+++ UsuarioDTO.java
@@ -0,0 +1,57 @@
+package com.ska.NEXUS.dto;
+
+public class UsuarioDTO {
+
+    private Long id;
+
+    private String nome;
+
+    private String senha;
+    
+    
+    private String usuario;
+    
+    private String email;
+
+    
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String teste) {
+        this.usuario = teste;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
