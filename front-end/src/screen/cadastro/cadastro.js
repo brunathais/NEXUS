67681c51fd37279e6d0c5d1762030a3942296350
brunathais <bruna_t_hermes@estudante.sesisenai.org.br>
2025-06-27 22:19:22 -0300

    Melhorando a parte de cadastro, validações de campos obrigatorios, não 100% ainda

--- front-end/src/screen/cadastro/cadastro.js
+++ front-end/src/screen/cadastro/cadastro.js
@@ -1,47 +1,77 @@
-    function efetuarLogin() {
-        //event.preventDefault(); // Evita o reload da página
+function obterValorCampo(id) {
+    return document.getElementById(id).value.trim(); //o trim() tira espaços em branco do inicio e fim
+}
 
-        // Obter valores dos campos do formulário
-        const varnome = document.getElementById("nome").value;
-        const email = document.getElementById("email").value;
-        const senha = document.getElementById("senha").value;
-        const confirmarSenha = document.getElementById("confirmar-senha").value;
+function obterDadosCadastro() {
+    return {
+        nome: obterValorCampo("nome"),
+        email: obterValorCampo("email"),
+        senha: obterValorCampo("senha"),
+        confirmarSenha: obterValorCampo("confirmarSenha")
+    }
+}
 
-        // Validação no frontend
-        if (senha !== confirmarSenha) {
-            alert("As senhas não coincidem!");
+function obterDadosLogin() {
+    return {
+        nome: obterValorCampo("nome"), //oq faz o :
+        senha: obterValorCampo("senha")
+    }
+}
+
+function efetuarLogin() {
+document.getElementById("loginForm").addEventListener("submit", function (event) {
+        event.preventDefault(); // Evita o reload da página, para usar o fetch
+        const { nome, senha } = obterDadosLogin(); //forma de desestruturação eu acho, pega as consts que estão na função
+
+        if (!nome || !senha) {
+            alert("Preencha os campos! Todos são obrigatórios para o Login!")
             return;
         }
-
-        // DTO que será enviado no corpo da requisição
-        const UsuarioDTO = {  //aqui junção do js com java
-            nome: varnome,
-            email: email,
-            senha: senha
-        };
-
-
-    // Configuração da requisição
-    fetch("http://localhost:8080/usuarios", {
-        method: "POST", // Método HTTP
-        headers: {
-            "Content-Type": "application/json", // Tipo de conteúdo enviado
-        },
-        body: JSON.stringify(UsuarioDTO), // Converte o DTO para JSON
     })
-        .then((response) => { //opcional
-            if (!response.ok) {
-                throw new Error("Erro ao cadastrar o usuário");
-            }
-            return response.text(); // Receber a mensagem de sucesso
-        })
-        .then((data) => {
-            alert(data); // Exibe a mensagem retornada pelo backend
-            // Redirecionar para o dashboard (caso necessário)
-            window.location.href = "../login/login.html";
-        })
-        .catch((error) => {
-            alert("Erro: " + error.message); // Exibe a mensagem de erro
-        });
+}
+
+function efetuarCadastro() {
+    const { nome, email, senha, confirmarSenha } = obterDadosCadastro();
+
+    if (!nome || !email || !senha || !confirmarSenha) { //! inverte o valor true e false
+        alert("Preencha os campos! Todos são obrigatórios para o Cadastro!")
+        return;
+    }
+}
+
+if (senha !== confirmarSenha) {
+    alert("As senhas são diferentes!");
+    return;
+}
+
+// DTO que será enviado no corpo da requisição
+const UsuarioDTO = {  //aqui junção do js com java
+    nome: varnome, // essa parte oq faz? de onde tem essas variaveis?
+    email: email,
+    senha: senha
+};
+
+// Configuração da requisição
+fetch("http://localhost:8080/usuarios", {
+    method: "POST", // Método HTTP
+    headers: {
+        "Content-Type": "application/json", // Tipo de conteúdo enviado
+    },
+    body: JSON.stringify(UsuarioDTO), // Converte o DTO para JSON
+})
+    .then((response) => { //opcional
+        if (!response.ok) {
+            throw new Error("Erro ao cadastrar o usuário");
+        }
+        return response.text(); // Receber a mensagem de sucesso
+    })
+    .then((data) => {
+        alert(data); // Exibe a mensagem retornada pelo backend
+        // Redirecionar para o dashboard (caso necessário)
+        window.location.href = "../login/login.html";
+    })
+    .catch((error) => {
+        alert("Erro: " + error.message); // Exibe a mensagem de erro
+    });
 }
 
--- front-end/src/screen/login/login.js
+++ front-end/src/screen/login/login.js
@@ -1,39 +1,40 @@
-document.getElementById("loginForm").addEventListener("submit", function (event) {
-    event.preventDefault(); // Evita o reload da página
+function efetuarLogin() {
+    document.getElementById("loginForm").addEventListener("submit", function (event) {
+        event.preventDefault(); // Evita o reload da página
 
-    // Obter valores dos campos do formulário
-    const usuario = document.getElementById("nome").value;
-    const senha = document.getElementById("senha").value;
+        // Obter valores dos campos do formulário
+        const usuario = document.getElementById("nome").value;
+        const senha = document.getElementById("senha").value;
 
-    // DTO que será enviado no corpo da requisição
-    const dto = {
-        usuario: usuario,
-        senha: senha
-       }
+        // DTO que será enviado no corpo da requisição
+        const dto = {
+            usuario: usuario,
+            senha: senha
+        }
 
-    // Configuração da requisição
+        // Configuração da requisição
 
-      fetch("http://localhost:8080/aulaLista/dados", {
-  //  fetch("http://localhost:8080/usuarioController/autenticar", {
-        method: "POST", // Método HTTP
-        headers: {
-            "Content-Type": "application/json", // Tipo de conteúdo enviado
-        },
-        body: JSON.stringify(dto), // Converte o DTO para JSON
-    })
-        .then((response) => { //opcional
-            if (!response.ok) {
-                throw new Error("Usuário ou senha inválidos");
-            }
-            return response.json(); // Receber a mensagem de sucesso
+        fetch("http://localhost:8080/aulaLista/dados", { //alterar rota
+            //  fetch("http://localhost:8080/usuarioController/autenticar", {
+            method: "POST", // Método HTTP
+            headers: {
+                "Content-Type": "application/json", // Tipo de conteúdo enviado
+            },
+            body: JSON.stringify(dto), // Converte o DTO para JSON
         })
-        .then((data) => {
-            alert(data); // Exibe a mensagem retornada pelo backend
-            // Redirecionar para o dashboard (caso necessário)
-            window.location.href = "index.html";
-        })
-        .catch((error) => {
-            alert("Erro: " + error.message); // Exibe a mensagem de erro
-        });
-});
-
+            .then((response) => { //opcional
+                if (!response.ok) {
+                    throw new Error("Usuário ou senha inválidos");
+                }
+                return response.json(); // Receber a mensagem de sucesso
+            })
+            .then((data) => {
+                alert(data); // Exibe a mensagem retornada pelo backend
+                // Redirecionar para o dashboard (caso necessário)
+                window.location.href = "index.html";
+            })
+            .catch((error) => {
+                alert("Erro: " + error.message); // Exibe a mensagem de erro
+            });
+    });
+}
