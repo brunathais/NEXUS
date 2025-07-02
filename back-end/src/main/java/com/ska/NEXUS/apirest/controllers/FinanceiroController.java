99111162cf292d2ecb8da7bfdef62e9b2aea386c
brunathais <bruna_t_hermes@estudante.sesisenai.org.br>
2025-07-01 22:19:09 -0300

    Exluido arquivos java

--- back-end/src/main/java/com/ska/NEXUS/apirest/controllers/FinanceiroController.java
+++
@@ -1,8 +0,0 @@
-/*package com.ska.NEXUS.apirest.controllers;
-
-@GetMapping("/resumo")
-public ResponseEntity<Map<String, Object>> getResumoFinanceiro() {
-    Map<String, Object> resumo = financeiroService.obterResumoFinanceiro();
-    return ResponseEntity.ok(resumo);
-}
-*/
--- back-end/src/main/java/com/ska/NEXUS/apirest/repository/TransacaoRepository.java
+++
@@ -1,8 +0,0 @@
-/*package com.ska.NEXUS.apirest.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.seuapp.model.Transacao;
-
-public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
-}
-*/
