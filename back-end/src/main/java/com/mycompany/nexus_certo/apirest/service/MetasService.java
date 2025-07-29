edfa550ced680bc8983ad2218bd520e15c962594
caroloestreich <caroline_oestreich@estudante.sesisenai.org.br>
2025-07-28 22:22:11 -0300

    FEAT- criando back-end java de metas 80% feito

--- back-end/src/main/java/com/mycompany/nexus_certo/apirest/service/MetasService.java
+++ back-end/src/main/java/com/mycompany/nexus_certo/apirest/service/MetasService.java
@@ -1,30 +1,33 @@
 package com.mycompany.nexus_certo.apirest.service;
 
-import com.mycompany.nexus_certo.apirest.model.MetasModel;
-import com.mycompany.nexus_certo.apirest.repository.MetasRepository;
 import java.util.List;
-import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.mycompany.nexus_certo.apirest.model.MetasModel;
+import com.mycompany.nexus_certo.apirest.repository.MetasRepository;
 
 @Service
 public class MetasService {
 
     @Autowired
+
     private MetasRepository repo;
-            
-    public MetasModel salvar(MetasModel metas){
+
+    public MetasModel salvar(MetasModel metas) {
         return repo.save(metas);
     }
-    public List<MetasModel> listar(){
-     return repo.findAll();
+
+    public List<MetasModel> listar() {
+        return repo.findAll();
     }
-    public Optional<MetasModel> buscarPorId(int id){
+
+    public void deletar(int id) {
+        return repo.deleteById(id);
+    }
+
+    public Optional<MetasModel> buscarPorId(int id) {
         return repo.findById(id);
     }
-    
-    public void deletar(int id){
-        repo.deleteById(id);
-    }
 }
-
