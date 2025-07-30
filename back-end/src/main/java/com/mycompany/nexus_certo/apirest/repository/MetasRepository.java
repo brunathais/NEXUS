eaa317525bda3f544a82b1493dabed0657fafafc
brunathais <bruna_t_hermes@estudante.sesisenai.org.br>
2025-07-29 21:24:13 -0300

    FEAT criando backend orçamento java e js funcional salvamento no banco e exibição de historico, só falta validação para o campo nome

--- back-end/src/main/java/com/mycompany/nexus_certo/apirest/repository/MetasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.nexus_certo.apirest.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mycompany.nexus_certo.apirest.model.MetasModel;
-
-public interface MetasRepository extends JpaRepository<MetasModel, Integer> {
-
-}
