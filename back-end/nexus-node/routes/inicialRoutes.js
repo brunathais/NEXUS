88d72f294e55adc5d88a32982f394010a1db7a0a
caroloestreich <caroline_oestreich@estudante.sesisenai.org.br>
2025-08-18 22:22:07 -0300

    Configuração do Docker

--- back-end/nexus-node/routes/inicialRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/graficosRoutes.js
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.send('Rota de gráficos funcionando!');
-});
-
-module.exports = router;
--- back-end/src/main/java/com/mycompany/nexus_certo/apirest/ApiRestApplication.java
+++ back-end/src/main/java/com/mycompany/nexus_certo/apirest/ApiRestApplication.java
@@ -3,12 +3,11 @@ package com.mycompany.nexus_certo.apirest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-    @SpringBootApplication
+@SpringBootApplication
 public class ApiRestApplication {
-    
 
     public static void main(String[] args) {
-        
+
         SpringApplication.run(ApiRestApplication.class, args);
     }
 }
